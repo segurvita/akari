9dd19b2137db93b990cc7791fa81c818f1dcf5c5
segur <segur.opus@gmail.com>
2018-12-03 15:01:07 +0900

    Add definition of Akari language to akari.rb

--- lib/akari.rb
+++ lib/akari.rb
@@ -1,6 +1,28 @@
+Encoding.default_external = "UTF-8"
 require "akari/version"
+require "r-fxxk"
 
 module Akari
   class Error < StandardError; end
-  # Your code goes here...
+
+  # define Akari language
+  class Akari < Brainfuck
+    nxt "ピロリンッ"
+    inc "ハロー！"
+    prv "悪い子ちゃん"
+    dec "ヤミ"
+    opn "いきま→しょう！"
+    cls "卍"
+    put "あ、カラスだ"
+    get "おけまる"
+  end
+
+  # read source code from first argument
+  program = ARGF.read
+
+  # display source code
+  puts program
+
+  # display result
+  puts Akari.new.fuck(program)
 end
