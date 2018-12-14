ef92b7029563201d730540212c0dbbd93f170d15
segur <segur.opus@gmail.com>
2018-12-14 13:21:45 +0900

    Fix encoding for argument

--- lib/akari/cli.rb
+++ lib/akari/cli.rb
@@ -6,12 +6,12 @@ module Akari
   class CLI < Thor
     desc "convert {akari_code}", "convert {akari_code} to {brainfxxk_code}"
     def convert(str)
-      Akari.convert(str)
+      Akari.convert(str.encode("UTF-8"))
     end
 
     desc "execute {akari_code}", "execute {akari_code}"
     def execute(str)
-      Akari.execute(str)
+      Akari.execute(str.encode("UTF-8"))
     end
   end
 end
