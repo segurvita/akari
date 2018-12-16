73362b5d305bec12a3e49079db189092bb2e6424
segur <segur.opus@gmail.com>
2018-12-16 10:34:11 +0900

    Change flow of reading file

--- lib/akari/cli.rb
+++ lib/akari/cli.rb
@@ -5,16 +5,14 @@ require "thor"
 module Akari
   class CLI < Thor
     desc "convert {akari_code_file}", "convert {akari_code} to {brainfxxk_code}"
-    def convert(str)
-      filename = str.encode("UTF-8")
-      filebody = File.read(filename).encode("UTF-8")
+    def convert(filename)
+      filebody = Akari.read_file(filename)
       puts Akari.convert(filebody)
     end
 
     desc "execute {akari_code_file}", "execute {akari_code}"
-    def execute(str)
-      filename = str.encode("UTF-8")
-      filebody = File.read(filename).encode("UTF-8")
+    def execute(filename)
+      filebody = Akari.read_file(filename)
       puts Akari.execute(filebody)
     end
   end
