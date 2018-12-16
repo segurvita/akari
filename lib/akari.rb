659122bff334bc9a9a1438f777b1df33c4b5ceae
segur <segur.opus@gmail.com>
2018-12-16 10:33:03 +0900

    Change argument name

--- lib/akari.rb
+++ lib/akari.rb
@@ -20,12 +20,12 @@ module Akari
   end
 
   # compile to Brainfuck
-  def self.convert(akari)
-    AkariSpec.new.compile(akari)
+  def self.convert(akari_code)
+    AkariSpec.new.compile(akari_code)
   end
 
   # run
-  def self.execute(akari)
-    AkariSpec.new.fuck(akari)
-  end  
+  def self.execute(akari_code)
+    AkariSpec.new.fuck(akari_code)
+  end
 end
